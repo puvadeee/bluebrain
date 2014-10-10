247bde5e8a4df4f59606e445c394735a6bee4403
Wayne Keenan <wayne@cannybots.com>
2014-10-10 17:11:49 +0100

    added interrupt example

---
+++ src/rfduino/HardwareTesting/Interrupts/Interrupts/Interrupts.ino
@@ -0,0 +1,30 @@
+
+#define COUNTER1_PIN 5
+#define COUNTER2_PIN 6
+
+volatile unsigned int counter1 = 0;
+volatile unsigned int counter2 = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(COUNTER1_PIN, INPUT);
+  pinMode(COUNTER2_PIN, INPUT);
+  attachPinInterrupt(COUNTER1_PIN, myPinCallback1, HIGH);
+  attachPinInterrupt(COUNTER2_PIN, myPinCallback2, HIGH);
+}
+
+void loop() {
+  Serial.print(counter1);
+  Serial.print(", ");
+  Serial.println(counter2);
+}
+
+int myPinCallback1(uint32_t ulPin) {
+  counter1++;
+  return 0;
+}
+
+int myPinCallback2(uint32_t ulPin){
+  counter2++;
+  return 0;
+}
