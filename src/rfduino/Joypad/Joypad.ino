421bdde6ecb122c4ae900bff69553f602613bd46
Wayne Keenan <wayne@cannybots.com>
2014-10-12 18:40:12 +0100

    Added zaxis to joypad sketch

--- src/rfduino/Joypad/Joypad.ino
+++ src/rfduino/Joypad/Joypad.ino
@@ -7,6 +7,7 @@
 // License: http://opensource.org/licenses/MIT
 //
 // Version:   1.0   -  14.08.2014  -  Inital Version 
+// Version:   1.1   -  14.08.2014  -  Inital Version 
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -15,9 +16,10 @@
 
 #include <RFduinoGZLL.h>
 
-#define XAXIS_PIN 2
-#define YAXIS_PIN 3
-#define BUTTON_PIN 4
+#define X_AXIS_PIN 2    // joypad x
+#define Y_AXIS_PIN 3    // joypad y
+#define Z_AXIS_PIN 5    // wheel throttle
+#define BUTTON_PIN 4    // joypad button 
 #define MSG_LEN 4
 
 #define GZLL_MAX_MSG_SIZE 32
@@ -26,7 +28,7 @@ char* dbgMsg = NULL;
 
 
 device_t role = DEVICE0;
-char msg[MSG_LEN] = {0};  // 4 bytes  = 3 bytes data 1 byte NULL terminator
+char msg[MSG_LEN+1] = {0};  // 4 bytes  = 4 bytes data 1 byte NULL terminator
 
 void setup()
 {
@@ -41,12 +43,13 @@ void setup()
 void loop()
 {
   bool buttonPressed = digitalRead(BUTTON_PIN)  & 0xFF;
-  uint8_t xAxis = 255-(analogRead(XAXIS_PIN)/4) ;          // scale from 0..1023 to 0-255 so data fits in a byte
-  uint8_t yAxis = 255-(analogRead(YAXIS_PIN)/4) ;
+  uint8_t xAxis = 255-(analogRead(X_AXIS_PIN)/4) ;          // scale from 0..1023 to 0-255 so data fits in a byte
+  uint8_t yAxis = 255-(analogRead(Y_AXIS_PIN)/4) ;
+  uint8_t zAxis = 255-(analogRead(Z_AXIS_PIN)/4) ;
   
-  snprintf(msg, MSG_LEN, "%c%c%c", xAxis, yAxis, buttonPressed );
+  snprintf(msg, MSG_LEN, "%c%c%c%c", xAxis, yAxis, buttonPressed, zAxis );
   
-  RFduinoGZLL.sendToHost((const char*)msg, MSG_LEN-1);      // don't bother sending the NULL byte at the end
+  RFduinoGZLL.sendToHost((const char*)msg, MSG_LEN);      // don't bother sending the NULL byte at the end
   //Serial.write((uint8_t*)msg, MSG_LEN);
   /*
   Serial.print(xAxis);
