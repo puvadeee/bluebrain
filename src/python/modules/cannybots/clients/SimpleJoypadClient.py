bd39470d0ba168ce48d57041ea64401828a47938
WayneKeenan <wayne@cannybots.com>
2014-10-12 19:54:41 +0100

    added simple joypad client

--- src/python/modules/cannybots/clients/SimpleJoypadClient.py
+++ src/python/modules/cannybots/clients/SimpleJoypadClient.py
@@ -1,1 +1,12 @@
 
+class SimpleJoypadClient:
+	
+	def __init__(self, bot):
+		self.bot=bot
+		
+	
+	def  updateJoypad(self, x,y,b):
+		x = map(x, -255, 255, 0,255)
+        y = map(y, -255, 255, 0,255)
+        msg = format(x,'02X') + format(y,'02X') + format(b, '02X') 
+        self.myBotDevice.sendHexString(msg)	
