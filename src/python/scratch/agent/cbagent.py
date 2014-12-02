6f49a79fec6b3b45b162caf7f24fa7adcb6fd651
WayneKeenan <wayne@cannybots.com>
2014-12-02 13:49:52 +0000

    added scratch integration and simple example

---
+++ src/python/scratch/agent/cbagent.py
@@ -0,0 +1,47 @@
+from scratra import *
+
+from time import sleep
+from cannybots.radio import BLE
+from cannybots.clients.joypad  import SimpleJoypadClient
+
+ble   = BLE() 
+myBot = ble.findNearest()   
+joypadClient = SimpleJoypadClient(myBot)
+
+
+motorASpeed = 0
+motorBSpeed = 0
+
+
+
+@start
+def whenstart(scratch):
+  print 'Scratch connection started.'
+
+@end
+def whenend(scratch):
+  print 'Scratch connection ended'
+
+
+@broadcast('CB_FORWARD')
+def forward(scratch):
+  print 'move forward'
+  joypadClient.updateJoypad(motorASpeed,motorASpeed,0)
+
+  scratch.broadcast("CB_HELLO!")
+
+@update('motorA_speed')
+def motorA_speed(scratch, value):
+  global motorASpeed
+  print 'motorA_speed:' + str(value)
+  motorASpeed = value
+
+
+@update('motorB_speed')
+def motorB_speed(scratch, value):
+  print 'motorB_speed:' + str(value)
+  motorBSpeed = value
+
+
+run(console=False)
+
