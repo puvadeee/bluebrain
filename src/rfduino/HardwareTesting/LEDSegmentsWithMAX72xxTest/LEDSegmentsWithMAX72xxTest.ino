4ecbcd6b4c0770f1388e3073723ac822a12c59d4
WayneKeenan <wayne@cannybots.com>
2014-12-22 17:23:43 +0000

    improved comments and added defined constants for readability

--- src/rfduino/HardwareTesting/LEDSegmentsWithMAX72xxTest/LEDSegmentsWithMAX72xxTest.ino
+++ src/rfduino/HardwareTesting/LEDSegmentsWithMAX72xxTest/LEDSegmentsWithMAX72xxTest.ino
@@ -1,17 +1,12 @@
-#include "LedControl.h"
-
 /*
-
 Library used: http://playground.arduino.cc/Main/LedControl
 
 HW used:
 Arduino:      http://www.pololu.com/product/3101
 LED Segment:  https://www.sparkfun.com/products/11405
-DRiver IC:    https://www.sparkfun.com/products/9622
+Driver IC:    https://www.sparkfun.com/products/9622
 
 Reference:    http://playground.arduino.cc/Main/MAX72XXHardware
-
-We have only a single MAX72XX.
 
 MAX7219 / MAX7221 IC Pinout:
 
@@ -19,31 +14,21 @@ MAX7219 / MAX7221 IC Pinout:
 )
 | 1   2   .  .  .  11  12
 
-Passivle component wiring:
 
-9  -> 100nf -> 19  (capacitor)
-9  -> 10uf  -> 19  (electrolytic capacitor)
-18  -> 330k    -> +ve (resistor, used to current limit the IC output t the LED's)
-19  +ve
-
-Wiring for: 
-A*   -> MAX72**
+Wiring for: A* pin  -> MAX72xx pin
 
 GND       4
 GND       9
-2         1  DIN
-8        13  CLK
-9        12  LOAD (CS)
+2         1  (DIN)
+8        13  (CLK
+9        12  (LOAD (CS))
 
-Wiringg for
-MAX72**   ->  LED 4 Segment DIsplay pin
+Wiring for: MAX72xx pin  ->  LED 4 Segment pin
 2       12    digit 0
 11       9    digit 1
 6        8    digit 2
 7        6    digit 3
-
 22       3    Decimal Point
-
 14      13    seg a
 16      7     seg b
 20      4     seg c
@@ -51,16 +36,25 @@ MAX72**   ->  LED 4 Segment DIsplay pin
 21      1     seg e
 15      10    seg f
 17      5     seg g
-*/
 
+Passive components wiring:
+
+9  -> 100nf -> 19  (capacitor)
+9  -> 10uf  -> 19  (electrolytic capacitor)
+18 -> 330k  -> +ve (resistor, used to current limit the IC output to the LED's)
+19 -> +ve
+
+*/
+#include "LedControl.h"
 
 #define SEGMENT_COUNT  4
+#define MAX72XX_COUNT  1
+#define DATA_PIN       2
+#define CLK_PIN        8
+#define LOAD_PIN       9
 
-
-LedControl lc = LedControl(2, 8, 9, 1);
-
-/* we always wait a bit between updates of the display */
-unsigned long delaytime = 50;
+#define DELAY_TIME      50
+LedControl lc = LedControl(DATA_PIN, CLK_PIN, LOAD_PIN, MAX72XX_COUNT);
 
 void setup() {
   /*
@@ -79,7 +73,7 @@ void displayTime(unsigned long time) {
     int d = ( time % (int)pow(10, SEGMENT_COUNT - i)  ) / pow(10, SEGMENT_COUNT - i - 1);
     lc.setDigit(0, i, d, i==0);
   }
-  delay(delaytime);
+  delay(DELAY_TIME);
   lc.clearDisplay(0);
 }
 
