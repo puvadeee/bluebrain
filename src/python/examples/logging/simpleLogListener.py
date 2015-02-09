55b2e7341621ee2ea7458d154190b963e91cefb1
waynekeenan <wayne@cannybots.com>
2015-02-09 12:30:20 +0000

    BLE notifications

--- src/python/examples/logging/simpleLogListener.py
+++ src/python/examples/logging/simpleLogListener.py
@@ -1,24 +1,28 @@
+import sys
+
+sys.path.insert(0, "modules")
 
 import time
 from cannybots.radio import BLE
 
+
 def myRecvFunc(bleuart, message):
-	print message
-	
+    print message
+
+
 def myOnDisconnectFunc(ble, message):
-	print message
-	
-	
+    print message
 
-ble = BLE() 
 
-myBot = ble.findByName('PingEchoBot')   
+ble = BLE()
+
+myBot = ble.findByName('PingEchoBot')
 myBot.addListener(myRecvFunc)
 myBot.onDisconnect(myOnDisconnectFunc)
 
-
-
-
-while True:	
-	time.sleep(1)
+while True:
+    time.sleep(3)
+    msg = format(1, '02X') + format(2, '02X') + format(3, '02X') + format(4, '02X')
+    print "sending: " + msg
+    #myBot.sendHexString(msg)
 
