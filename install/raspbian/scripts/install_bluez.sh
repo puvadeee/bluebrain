16f69d514eac8ca8e4dfd59223e321b90cb51f29
waynekeenan <wayne@cannybots.com>
2015-02-25 18:10:26 +0000

    added dbus examples

--- install/raspbian/scripts/install_bluez.sh
+++ install/raspbian/scripts/install_bluez.sh
@@ -9,4 +9,5 @@ cd vendor/bluez/bluez-5.28
 make install && ln -svf ../libexec/bluetooth/bluetoothd /usr/sbin
 install -v -dm755 /etc/bluetooth && install -v -m644 src/main.conf /etc/bluetooth/main.conf
 install -v -dm755 /usr/share/doc/bluez-5.28 && install -v -m644 doc/*.txt /usr/share/doc/bluez-5.28
-cp -v attrib/gatttool /usr/local/bin/
+install -c attrib/gatttool /usr/local/bin/gatttool
+#cp -v attrib/gatttool /usr/local/bin/
