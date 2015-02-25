26e56e41a86f3a2f4a5aadd9c8e460f1e4725ae9
waynekeenan <wayne@cannybots.com>
2015-02-25 10:28:28 +0000

    reduce default camera fps

--- install/raspbian/rootfs/opt/cannybots/runtime/camera/start.sh
+++ install/raspbian/rootfs/opt/cannybots/runtime/camera/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH="$(pwd)"
-./mjpg_streamer -i "./input_raspicam.so -fps 25 -q 50 -x 640 -y 480" -o "./output_http.so -p 9000 -w ./www"
+./mjpg_streamer -i "./input_raspicam.so -fps 15 -q 50 -x 640 -y 480" -o "./output_http.so -p 9000 -w ./www"
 
 
 
