ecc18d624ef7cdf83db74c08f2e70c500805d3ff
waynekeenan <wayne.keenan@gmail.com>
2015-05-29 11:33:34 +0000

    Beat syncronised dancing Cannybot with Sonic Pi

--- src/sonicpi/StayinAlive/CannybotsStayinAlive.rb
+++ src/sonicpi/StayinAlive/CannybotsStayinAlive.rb
@@ -1,23 +1,35 @@
 # Cannybots - Stayin Alive!
 
-use_sample_pack "/Users/wayne//projects/SonicPiDancing/samples/"
+# Move a Cannybot forward to the beat!
+
+
+# Websocket Client used to send messages to the the Cannybows WebSocket API
+# TODO: move this to a Cannybots Ruby GEM
+require 'json'      
+require 'websocket-client-simple'
+ws = WebSocket::Client::Simple.connect 'ws://localhost:3141/api/ws/cannybots'
+
+
+
+steps = 51.5   # tweak this to match the funky beat, BeeGee's have a good drummer :)
 
 define :play_sample_portion do |samp, s ,e|
   sample samp, start: s, finish: e
+  # Send the movement commands... pretty basic for now...
+  # this is gonna get way more interesting using loopkup tables and a counter 
+  # for multiple Cannybot dancers more robo moves on the dancefloor :)
+  ws.send JSON.generate({:botId => '0', :rawBytes => "f"})   
 end
 
 
-steps = 51.5
+use_sample_pack "/home/pi/cannybots/bluebrain/src/sonicpi/StayinAlive/samples"
 samp=:StayinAlive
 
-
 sampleLen = sample_duration samp
-
 sampleStepDelay  = sampleLen/steps
-
 stepSize = 1.0/steps
-pos = 0.0
 
+pos = 0.0
 (steps-1).times do
   print pos
   play_sample_portion(samp, pos, pos+stepSize)
