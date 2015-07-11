0c7c133d7c2574f10fbfcb0635f493288c301e75
WayneKeenan <wayne@cannybots.com>
2015-07-11 10:49:36 +0000

    moved scratch agent into a module

--- install/raspbian/scripts/install_pythonlibs.sh
+++ install/raspbian/scripts/install_pythonlibs.sh
@@ -2,5 +2,6 @@
 cd vendor/cannybots
 tar zxvf Cannybots-*.tar.gz
 cd Cannybots-*
+rm -fr /usr/local/lib/python2.7/dist-packages/cannybots/
 sudo python setup.py install
 
--- src/python/modules/setup.py
+++ src/python/modules/setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
 	name='Cannybots',
-	version='0.3dev',
+	version='0.4dev',
 	packages=['cannybots','cannybots.clients'],
 	license='MIT license',
 	author="Wayne Keenan",
 	author_email="wayne@cannybots.com",
-	url="http://www.cannybots.com",
+	url="http://www.cannybots.com/",
 	long_description=open('README.txt').read(),
 )
