c68f8cb357652bfc0a13e0cd8dfe1a0260b60d32
WayneKeenan <wayne@cannybots.com>
2015-09-13 22:04:49 +0000

    Bluetooth LE scan workaround for Noble Node BLE plugin

--- install/raspbian/scripts/install_rootfs.sh
+++ install/raspbian/scripts/install_rootfs.sh
@@ -1,18 +1,15 @@
 #!/bin/bash 
 
-cp -rv  rootfs/* /
-cp -rv  ../../src/python/apps/*  /opt/cannybots/runtime/apps/
-chown -R pi  /opt/cannybots/runtime
-
-cp -rv  ../../src/python/scratch/agent  /opt/cannybots/runtime/scratch/
+#cp -rv  rootfs/* /
+#cp -rv  ../../src/python/apps/*  /opt/cannybots/runtime/apps/
+#chown -R pi  /opt/cannybots/runtime
 
 SCRATCH_PRJ_DIR=/home/pi/Documents/Scratch\ Projects/Cannybots
 mkdir "$SCRATCH_PRJ_DIR"
 cp -v   ../../src/scratch/* "$SCRATCH_PRJ_DIR"/
 chown -R pi.pi "$SCRATCH_PRJ_DIR"
  
-sudo update-rc.d avahi-daemon defaults
-update-rc.d xboxdrv defaults
-update-rc.d xboxdrv start
+#update-rc.d xboxdrv defaults
+#update-rc.d xboxdrv start
 
-svc -du /etc/service/*
+#svc -du /etc/service/*
